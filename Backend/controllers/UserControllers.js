c4e53078359e08bd276d3cdff763ef2210ef3101
Raushan <roushankumarsingh2018@gmail.com>
2024-06-22 11:15:36 +0530

    Added encryption  for password

--- Backend/controllers/UserControllers.js
+++ Backend/controllers/UserControllers.js
@@ -1,4 +1,5 @@
 const User = require("../models/UserModel");
+const bcrypt = require("bcrypt");
 
 const UserControllers = {
   createUser: async (req, res) => {
@@ -7,9 +8,14 @@ const UserControllers = {
     try {
       const userEmail = await User.findOne({ where: { email: email } });
       if (userEmail === null) {
-        // console.log("Not found!");
-        const newUser = await User.create({ name, email, password });
-        res.status(201).json(newUser);
+        // bcrypt.hash(password, saltrounds, async (err, hash) => { //saltround to create a random string how many times i will randomize, so that string along with it's password is hash so normal is 10 times , 10 times randomize and get a string
+        // more the salrtounds the less possibility of the do strings matching so if u have 1million user there is less chance to match password u can do 20, 30 anything
+        bcrypt.hash(password, 10, async (err, hash) => {
+          console.log("Error in bcrypt.hash ", err);
+          console.log("Hash======> ", hash);
+          const newUser = await User.create({ name, email, password: hash });
+          res.status(201).json(newUser);
+        });
       } else {
         console.log("User already exist ");
         res.status(401).json({ err: "User already exist" });
@@ -20,17 +26,22 @@ const UserControllers = {
     }
   },
   loginUser: async (req, res) => {
-    const { name,email, password } = req.body;
+    const { name, email, password } = req.body;
     console.log(req.body);
     try {
-      const userVerify = await User.findOne({
-        where: { email: email, password: password },
+      const userVerifyEmail = await User.findOne({
+        where: { email: email },
       });
-      if (userVerify === null) {
+      const userVerifyPassword = await User.findOne({
+        where: { password: password },
+      });
+
+      if (userVerifyEmail === null) {
         res.status(403).json({ err: "User doesn't exist" });
       } else {
-        console.log("User  exist  ");
-        res.status(200).json({ msg: "User login status is true now" });
+        userVerifyPassword === null
+          ? res.status(403).json({ err: "Password doesn't match" })
+          : res.status(200).json({ msg: "User login status is true now" });
       }
     } catch (err) {
       console.log("Error in Logging in  user ", err);
--- Backend/models/UserModel.js
+++ Backend/models/UserModel.js
@@ -1,10 +1,13 @@
 const sequelize = require("../utils/db");
+
 const { DataTypes } = require("sequelize");
 
 const user = sequelize.define("user", {
   id: {
-    type: DataTypes.UUID,
-    defaultValue: DataTypes.UUIDV4,
+    type: DataTypes.INTEGER,
+    autoIncrement:true,
+    defaultValue:DataTypes.UUIDV4,
+    allowNull:false,
     primaryKey: true,
   },
   name: {
@@ -23,3 +26,21 @@ const user = sequelize.define("user", {
 });
 
 module.exports = user
+
+// string password -> encrypt -> hash value  . what we save in db (this is one way incryption) once we encrypt it we get a hash but i can never go back to the password again
+// there are some algorithm 
+// the algo we are using is called Blowfish encryption but this also has some problem this is older way of doing this what essentially happens is that  if we put the password 12345678 or similar to this when i incrypt this we get a hash 
+// if 2 user suppose put same password
+
+// 12345678 -> Encrypt -> hasha (anything like "kdoeko")
+// 12345678 -> Encrypt -> hasha
+
+//so then engineer start adding salt  => Encrypt + salt(saltis small string basically)  if u even put the same password u get the different output always
+//  user A  password => 123456 + salt -> 12345 + 'lsie'
+//  user B  password => 123456 + salt -> 12345 + 'jdje'
+// whatever password we get we gonna salt it and then encrypt it through blowfish incryption it's called bcrypt(blowfish incrypt) and whatever hash we get we gonna save it in the db so that's going to the signup section 
+// code is very easy we just use bcrypt library
+// just install npm i bcrypt library and that's it
+// salt is to create a random string so how many time i can randomize normal is 10 times 
+
+// then next thing is login is not work now u can't match the password for match u have to write a algorith for that
